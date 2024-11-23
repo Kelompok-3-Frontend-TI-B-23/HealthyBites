dbdca146f57cbaa45e58d33154ce143001c58fcd
eilleendwe <eilleenyeoh3@gmail.com>
2024-11-23 21:46:17 +0700

    Membuat backend untuk create account. login dan logout
    - membuat create account dan memasukkan data user ke dalam mongodb (termasuk autentikasi dan hash password)
    - membuat fitur login
    - membuat fitur logout
    - membuat front end simple untuk halaman login dan create account untuk mengecek logika create account, login dan logout (nanti akan dikembangkan)

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoute');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/createAccount.html');
+});
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+  
+// Koneksi MongoDB
+const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+mongoose.connect(dbURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+// Route bruno
+app.use('/api/users', userRoutes);
+
+// Sever jalan
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
